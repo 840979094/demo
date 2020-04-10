a3fb09e272e409f8f915a8c9edc575ee1cb8c02f
heaven <840979094@qq.com>
2020-04-10 18:37:35 +0800

    小明

---
+++ src/components/XiaoMing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>我是xiaoming</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Heaven',
+    data() {
+      return {
+        msg: 'heaven',
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
